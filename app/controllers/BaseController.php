3fbc93841827c1d74f251daddc531ba95d666d27
Max Lau <forevervmax@gmail.com>
2014-03-13 19:20:52 +0800

    add global responser,to support json response and html response

--- app/controllers/BaseController.php
+++ app/controllers/BaseController.php
@@ -11,11 +11,46 @@ class BaseController extends Controller {
 
     protected $userId;
 
+    protected $responser = array(
+        'msg'=>'',
+        'error'=>false,
+        'data'=>array(),
+        'redirect'=> '',
+        'viewPath'=>''
+    );
+
 
     public function __construct(){
-
         if (Auth::check()){
             $this->userId = Auth::user()->getAuthIdentifier();
+        }
+    }
+
+    protected function responses($responser){
+        if(Request::wantsJson()){
+            if($responser['error']){
+                return Response::json(array(
+                    'msg'=>$responser['msg'],
+                    'error'=>$responser['error'],
+                    'data'=>$responser['data'])
+                );
+            }else{
+                return Response::json(array(
+                    'msg'=>$responser['msg'],
+                    'error'=>$responser['error'],
+                    'data'=>$responser['data']
+                ));
+            }
+        }else{
+            if($responser['error']){
+                return Redirect::to($responser['redirect'])->with(array('error'=>$responser['msg']));
+            }else{
+                if (Request::isMethod('get')){
+                    $this->layout->content = View::make($responser['viewPath'], $responser['data']);
+                }else{
+                    return Redirect::to($responser['redirect']);
+                }
+            }
         }
     }
 
