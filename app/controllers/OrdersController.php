24f4ca804bb8edbabb43349bbe746007cb1e3f4f
Max Lau <forevervmax@gmail.com>
2014-03-14 14:39:33 +0800

    restrurct order

--- app/controllers/OrdersController.php
+++ app/controllers/OrdersController.php
@@ -1,107 +1,126 @@
 <?php
 
-class OrdersController extends \BaseController {
+class OrdersController extends \BaseController
+{
 
-	/**
-	 * Display a listing of the resource.
-	 *
-	 * @return Response
-	 */
-	public function index()
-	{
-        $orders = Order::with(['address'])->where('user_id','=',$this->userId)->orderBy('created_at','desc')->get();
-        $this->layout->content = View::make('orders.list',compact('orders'));
-	}
+    /**
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
 
-	/**
-	 * Show the form for creating a new resource.
-	 *
-	 * @return Response
-	 */
-	public function create()
-	{
-		//
-	}
 
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @return Response
-	 */
-	public function store()
-	{
+    public function index()
+    {
+        $orders = Order::with(['address'])->where('user_id', '=', $this->userId)->orderBy('created_at', 'desc')->get();
+        $this->responser['data'] = compact('orders');
+        $this->responser['viewPath'] = 'orders.list';
+        return $this->responses($this->responser);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @return Response
+     */
+    public function store()
+    {
         $input = Input::only(['ship_to']);
 
-        if($address = Address::find($input['ship_to'])){
-            if($address->user_id != $this->userId){
-              return $this->responses('该地址不属于用户',true,array(),url('checkout'));
+        if ($address = Address::find($input['ship_to'])) {
+            if ($address->user_id != $this->userId) {
+                $this->responser['error'] = true;
+                $this->responser['msg'] = '该地址不属于此用户';
+                $this->responser['redirect'] = url('checkout');
             }
 
             $order = new Order();
             $order->newOrder($input['ship_to']);
-            return Redirect::route('orders.index');
 
-        }else{
-            return $this->responses('该地址不存在',true,array(),url('checkout'));
+            $this->responser['redirect'] = route('orders.index');
+
+        } else {
+
+            $this->responser['error'] = true;
+            $this->responser['msg'] = '该地址不存在';
+            $this->responser['redirect'] = url('checkout');
         }
-	}
 
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function show($id)
-	{
-       $order = Order::find($id);
-       $orderItems =  OrderItem::where('order_id','=',$id)->get();
-       $this->layout->content = View::make('orders.info',compact('orderItems','order'));
-	}
+        return $this->responses($this->responser);
+    }
 
-	/**
-	 * Show the form for editing the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function edit($id)
-	{
-		//
-	}
+    /**
+     * Display the specified resource.
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function show($id)
+    {
+        $order = Order::find($id);
+        $orderItems = OrderItem::where('order_id', '=', $id)->get();
 
-	/**
-	 * Update the specified resource in storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function update($id)
-	{
-		//
-	}
+        $this->responser['viewPath'] = 'orders.info';
+        $this->responser['data'] = compact('orderItems','order');
 
-	/**
-	 * Remove the specified resource from storage.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function destroy($id)
-	{
-		//
-	}
+        return $this->responses($this->responser);
+    }
 
-    public function getCheckout(){
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function update($id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+
+    public function getCheckout()
+    {
 
         $cart = new Cart();
         $cartList = $cart->contents();
         $totalItems = $cart->totalItems();
         $totalPrice = $cart->totalPrice();
-        $address = Address::where('is_default','=',1)->where('user_id','=',$this->userId)->first();
+        $address = Address::where('is_default', '=', 1)->where('user_id', '=', $this->userId)->first();
 
+        $this->responser['viewPath']= 'orders.checkout';
+        $this->responser['data'] = compact('cartList', 'address', 'totalItems', 'totalPrice');
 
-        $this->layout->content = View::make('orders.checkout',compact('cartList','address','totalItems','totalPrice'));
+        return $this->responses($this->responser);
     }
-
 }
