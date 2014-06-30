27285a59c231404e4085e1d0a786c9d4779abe65
Max Lau <forevervmax@gmail.com>
2014-06-30 13:29:00 +0800

    fix error links

--- app/views/partials/header.blade.php
+++ app/views/partials/header.blade.php
@@ -3,7 +3,7 @@
     <li><a href="/products">商品列表</a></li>
     <li><a href="/carts">我的购物车</a></li>
     <li><a href="/orders">我的订单</a></li>
-    <li><a href="/address">我的地址</a></li>
+    <li><a href="/addresses">我的地址</a></li>
     @if (Auth::check())
     <li><a href="/user/logout">欢迎 {{Auth::user()->username}}! 登出</a></li>
     @else
